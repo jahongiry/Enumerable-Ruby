ee6e01c843b74e8d1ebbf5640cd46c07f56a42e9
Mohammed Raad Zahraw Al-Suwayyid <mohamedraad77@gmail.com>
2022-08-26 16:12:38 +0300

    Add my enumrable module

---
+++ enumerable.rb
@@ -0,0 +1,30 @@
+module MyEnumerable
+  def any?
+    return 'No block supplied' unless block_given?
+
+    each do |item|
+      return true if yield item
+    end
+
+    false
+  end
+
+  def all?
+    return 'No block supplied' unless block_given?
+
+    each { |item| return false unless yield item }
+
+    true
+  end
+
+  def filter
+    rt = []
+    return rt.to_enum unless block_given?
+
+    each do |item|
+      rt.push(item) if yield item
+    end
+
+    rt
+  end
+end
